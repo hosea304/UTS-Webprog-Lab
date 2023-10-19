79765e91e6c8ef3d8a44a4ff48f1b5053dea3054
Mesayaaa <127949657+Mesayaaa@users.noreply.github.com>
2023-10-19 16:32:01 +0700

    .

--- login.php
+++ login.php
@@ -7,7 +7,7 @@ session_start();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <?php include('bootstrap.php') ?>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <title>Login</title>
 </head>
 
@@ -32,6 +32,7 @@ session_start();
             <button type="submit" class="w-25 py-2 mx-auto mt-5 btn btn-primary rounded-3 custom-button">Login</button>
         </form>
     </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
 
 </html>
