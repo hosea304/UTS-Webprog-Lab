b1bf7b35e766493cad90c2e1dbb546873e4a057f
Hosea <hoseahose63@gmail.com>
2023-10-21 14:38:04 +0700

    tambah navbar di edit

--- update/edit.php
+++ update/edit.php
@@ -3,21 +3,21 @@ session_start();
 $koneksi = mysqli_connect("localhost", "root", "", "todo");
 
 if (mysqli_connect_errno()) {
-    die("Koneksi database gagal: " . mysqli_connect_error() . "(" . mysqli_connect_errno() . ")");
+  die("Koneksi database gagal: " . mysqli_connect_error() . "(" . mysqli_connect_errno() . ")");
 }
 
 // Retrieve user_id from the session
 $user_id = isset($_SESSION['user_id']) ? $_SESSION['user_id'] : null;
 
 if ($user_id === null) {
-    die("User ID not found in the session.");
+  die("User ID not found in the session.");
 }
 
 if (isset($_POST['submit'])) {
-    $id = $_POST['id'];
-    $tugas = $_POST['tugas'];
-    $priority = $_POST['priority'];
-    $status = $_POST['status'];
+  $id = $_POST['id'];
+  $tugas = $_POST['tugas'];
+  $priority = $_POST['priority'];
+  $status = $_POST['status'];
 
   switch ($priority) {
     case 'High':
@@ -59,35 +59,38 @@ $hasil = mysqli_query($koneksi, $sql);
 </head>
 
 <body>
+  <?php include('../navigation.php') ?>
   <div class="container-fluid">
     <div class='d-flex justify-content-center align-items-center w-100 h-100 flex-column'>
       <h1 class="mb-3">To-do list</h1>
       <form action="edit.php" method='POST' class="mb-3">
-      <div class="rounded-3 p-2" style=" box-shadow: 5px 5px 5px #888888; background-color: #FFF2D8">
-      <div class="container text-center">
-        <label>New To Do</label>
-        <input type="hidden" name="id" id="id" required style="border: 1px solid #ccc; border-radius: 5px; padding: 3px; width: 20%; transition: border-color 0.3s, box-shadow 0.3s;"
-        onmouseover="this.style.borderColor = '#007bff'; this.style.boxShadow = '0 0 5px #007bff';"
-        onmouseout="this.style.borderColor = '#ccc'; this.style.boxShadow = 'none';">
-        <input type="text" name="tugas" id="tugas" required style="border: 1px solid #ccc; border-radius: 5px; padding: 3px; width: 20%; transition: border-color 0.3s, box-shadow 0.3s;"
-          onmouseover="this.style.borderColor = '#007bff'; this.style.boxShadow = '0 0 5px #007bff';"
-          onmouseout="this.style.borderColor = '#ccc'; this.style.boxShadow = 'none';">
-          <select name="priority" id="priority" style="border-radius: 5px;">
-          <option value="High">High</option>
-          <option value="Medium">Medium</option>
-          <option value="Low">Low</option>
-        </select>
-        <select name="status" id="status" style="border-radius: 5px;">
-          <option value="No Status">No Status</option>
-          <option value="On Progress">On Progress</option>
-          <option value="Done">Done</option>
-        </select>
-        <input type="submit" value="Save" class="Add rounded-3" name="submit">
-</div>
+        <div class="rounded-3 p-2" style=" box-shadow: 5px 5px 5px #888888; background-color: #FFF2D8">
+          <div class="container text-center">
+            <label>New To Do</label>
+            <input type="hidden" name="id" id="id" required
+              style="border: 1px solid #ccc; border-radius: 5px; padding: 3px; width: 20%; transition: border-color 0.3s, box-shadow 0.3s;"
+              onmouseover="this.style.borderColor = '#007bff'; this.style.boxShadow = '0 0 5px #007bff';"
+              onmouseout="this.style.borderColor = '#ccc'; this.style.boxShadow = 'none';">
+            <input type="text" name="tugas" id="tugas" required
+              style="border: 1px solid #ccc; border-radius: 5px; padding: 3px; width: 20%; transition: border-color 0.3s, box-shadow 0.3s;"
+              onmouseover="this.style.borderColor = '#007bff'; this.style.boxShadow = '0 0 5px #007bff';"
+              onmouseout="this.style.borderColor = '#ccc'; this.style.boxShadow = 'none';">
+            <select name="priority" id="priority" style="border-radius: 5px;">
+              <option value="High">High</option>
+              <option value="Medium">Medium</option>
+              <option value="Low">Low</option>
+            </select>
+            <select name="status" id="status" style="border-radius: 5px;">
+              <option value="No Status">No Status</option>
+              <option value="On Progress">On Progress</option>
+              <option value="Done">Done</option>
+            </select>
+            <input type="submit" value="Save" class="Add rounded-3" name="submit">
+          </div>
       </form>
-</div>
+    </div>
 
-<div class="rounded-3 p-3" style=" box-shadow: 5px 5px 5px #888888; background-color: #FFF2D8">
+    <div class="rounded-3 p-3" style=" box-shadow: 5px 5px 5px #888888; background-color: #FFF2D8">
       <div class="table-container">
         <div class="table-container">
           <table class="table scrollable-table">
@@ -146,45 +149,45 @@ $hasil = mysqli_query($koneksi, $sql);
         </div>
       </div>
     </div>
-            </div>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <script src="../script/script.js"></script>
-    <script>
-      var urlString = window.location.href;
-      var url = new URL(urlString);
-      var id = url.searchParams.get("id");
-      document.getElementById("id").value = id;
+  </div>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="../script/script.js"></script>
+  <script>
+    var urlString = window.location.href;
+    var url = new URL(urlString);
+    var id = url.searchParams.get("id");
+    document.getElementById("id").value = id;
 
-      var tugas = url.searchParams.get("tugas");
-      document.getElementById("tugas").value = tugas;
+    var tugas = url.searchParams.get("tugas");
+    document.getElementById("tugas").value = tugas;
 
-      var status = url.searchParams.get("status");
-      document.getElementById("status").value = status;
+    var status = url.searchParams.get("status");
+    document.getElementById("status").value = status;
 
-      var priority = url.searchParams.get("priority");
+    var priority = url.searchParams.get("priority");
 
-      if (priority == 1) {
-        priority = "Low";
-      } else if (priority == 2) {
-        priority = "Medium";
-      } else {
-        priority = "High";
-      }
-      document.getElementById("priority").value = priority;
-    </script>
+    if (priority == 1) {
+      priority = "Low";
+    } else if (priority == 2) {
+      priority = "Medium";
+    } else {
+      priority = "High";
+    }
+    document.getElementById("priority").value = priority;
+  </script>
 
-    <script>
-      const deleteButtons = document.querySelectorAll('.delete');
-      deleteButtons.forEach(button => {
-        button.addEventListener('click', function (event) {
-          const confirmation = confirm('Apakah Anda yakin ingin menghapus item ini?');
-          if (!confirmation) {
-            event.preventDefault();
-          }
-        });
+  <script>
+    const deleteButtons = document.querySelectorAll('.delete');
+    deleteButtons.forEach(button => {
+      button.addEventListener('click', function (event) {
+        const confirmation = confirm('Apakah Anda yakin ingin menghapus item ini?');
+        if (!confirmation) {
+          event.preventDefault();
+        }
       });
+    });
 
-    </script>
+  </script>
 </body>
 
 </html>
