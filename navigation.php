2a4b561ff0a461a1babf0b054b5f765c306c1cc1
Mesayaaa <127949657+Mesayaaa@users.noreply.github.com>
2023-10-22 05:43:03 +0700

    style navi

--- navigation.php
+++ navigation.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand bg-white py-3 px-5">
     <div class="container justify-content-between">
-        <div class="w-25 d-flex justify-content-around">
+        <div class="w-25">
             <?php
             if (isset($_SESSION['user_id']) && !empty($_SESSION['user_id'])) {
                 ?>
